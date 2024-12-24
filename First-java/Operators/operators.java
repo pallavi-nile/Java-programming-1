be701d5a1041736771a47bd58c45b0db724555d1
pallavi-nile <nile.p@northeastern.edu>
2024-12-23 19:58:46 -0500

    Operations
    Multiplication , division , assigning values and increment

--- First-java/Operators/operators.java
+++ First-java/Operators/operators.java
@@ -14,6 +14,26 @@ public class operators {
         int result2 = num1 - num2;
         System.out.println(result2);
 
+        //Multiply  two numbers 
+        int result3 = num1*num2;
+        System.out.println(result3);
+
+        //Divide two numbers 
+        int result4 = num1/num2;
+        System.out.println("Division is " + result4);
+
+        // incrementing values
+
+        //num1= num1+2;
+        
+
+        //num1 = num1+2;
+        //num1 +=1;
+        num1++ ; // increment
+
+        System.out.println(num1);
+
+
 
     }
 
