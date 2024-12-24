bfc881439eb737dbee5fc3b34dde8078239511b3
pallavi-nile <nile.p@northeastern.edu>
2024-12-24 13:39:13 -0500

    Created Logical.java file
    Operations using logical operators

---
+++ First-java/Operators/Logical.java
@@ -0,0 +1,21 @@
+package Operators;
+
+public class Logical 
+{
+
+    public static void main (String args[])
+    {
+        int x = 4;
+        int y = 9;
+        int a = 6;
+        int b = 3;
+
+        boolean result = x <y && a>b ;
+        System.out.println(result);
+
+
+        
+
+    }
+    
+}
