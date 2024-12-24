97abbc865df1c94f290685694533181d64b9c985
pallavi-nile <nile.p@northeastern.edu>
2024-12-24 13:13:23 -0500

    changes made in Comparison.java
    == operator and != operator

--- First-java/Operators/Comparison.java
+++ First-java/Operators/Comparison.java
@@ -2,11 +2,44 @@ package Operators;
 
 public class Comparison {
     public static void main(String args[]){
+
+        // check > and < operators 
         int x= 6;
         int y=5;
 
-        boolean result = x<y;
-        System.out.println(result);
+        boolean result1 = x<y;
+        System.out.println(result1);
+
+        boolean result2 = x>y;
+        System.out.println(result2);
+
+        // checking == operator 
+
+        int a = 3;
+        int b = 3;
+
+        boolean result3= a>=b;
+        System.out.println(result3);
+
+        boolean result4= a==b;
+        System.out.println(result4);
+
+        boolean result5= x==y;
+        System.out.println(result5);
+
+        // checking != operator 
+
+        boolean result6= x!=y;
+        System.out.println(result6);
+
+        boolean result7= a!=b;
+        System.out.println(result7);
+        
+        
+
+
+
+    
     }
     
 }
